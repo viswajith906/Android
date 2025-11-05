3f0aa5fc10706ac67ee2a841bcef97f79a76e64a
viswajith906 <viswajith906@gmail.com>
2025-11-05 11:35:16 +0530

    Update 07_MainActivity.java

--- 07-Explicit Intents/07_MainActivity.java
+++ 07-Explicit Intents/07_MainActivity.java
@@ -8,14 +8,20 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main); }
-public void onIndexSendButtonClicked(View view)
-{
-Intent intent=new Intent(MainActivity.this,MainActivity2.class);
-EditText editText=(EditText) findViewById(R.id.EditTextIndex);
-String index=editText.getText().toString();
-intent.putExtra("ImageIndex",index);
-startActivity(intent); }}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnIndexSendButtonClicked(View view){
+        Intent intent=new Intent(this,MainActivity2.class);
+        EditText editText=(EditText) findViewById(R.id.EditTextIndex);
+        String index=editText.getText().toString();
+        intent.putExtra("ImageIndex",index);
+        startActivity(intent);
+
+    }
+
+}
