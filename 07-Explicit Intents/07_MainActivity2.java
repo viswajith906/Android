fdeef2fe6a17b949dd9dc1ba9f511b85933d9406
viswajith906 <viswajith906@gmail.com>
2025-11-05 10:27:51 +0530

    Update 07_MainActivity2.java

--- 07-Explicit Intents/07_MainActivity2.java
+++ 07-Explicit Intents/07_MainActivity2.java
@@ -6,32 +6,39 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        Bundle bundle=getIntent().getExtras();
-        if(bundle!=null){
-            String imgindex= bundle.getString("ImageIndex");
-            setImage(imgindex);
-        }
-    }
-    private void setImage(String index){
-        ImageView imageView=(ImageView) findViewById(R.id.imageViewCountry);
-        switch (index){
-            case "1":
-                imageView.setImageResource(R.drawable.india);
-                break;
-            case "2":
-                imageView.setImageResource(R.drawable.sa);
-                break;
-            case "3":
-                imageView.setImageResource(R.drawable.nz);
-                break;
-            case "4":
-                imageView.setImageResource(R.drawable.aus);
-                break;
-        }
-    }
+@Override
+protected void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main2);
+Bundle bundle=getIntent().getExtras();
+if (bundle!=null)
+{
+String ImgIndex=bundle.getString("ImageIndex");
+setImage(ImgIndex);
+}
+}
+private void setImage(String index)
+{
+ImageView imageView=findViewById(R.id.ImageViewCountry);
+if (index == null || index.isEmpty()) {
+imageView.setImageResource(R.drawable.img1);
+return;
+}
+switch (index)
+{
+case "1":
+imageView.setImageResource(R.drawable.img1);
+break;
+case "2":
+imageView.setImageResource(R.drawable.img2);
+break;
+case "3":
+imageView.setImageResource(R.drawable.img3);
+break;
+case "4":
+imageView.setImageResource(R.drawable.img4);
+break;
+}
+}
 }
