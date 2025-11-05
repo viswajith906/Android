85d592497c896217bdf60ea544cfb7e28b590f5a
viswajith906 <viswajith906@gmail.com>
2025-11-05 11:37:41 +0530

    Update 07_MainActivity2.java

--- 07-Explicit Intents/07_MainActivity2.java
+++ 07-Explicit Intents/07_MainActivity2.java
@@ -6,39 +6,32 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-@Override
-protected void onCreate(Bundle savedInstanceState)
-{
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_main2);
-Bundle bundle=getIntent().getExtras();
-if (bundle!=null)
-{
-String ImgIndex=bundle.getString("ImageIndex");
-setImage(ImgIndex);
-}
-}
-private void setImage(String index)
-{
-ImageView imageView=findViewById(R.id.ImageViewCountry);
-if (index == null || index.isEmpty()) {
-imageView.setImageResource(R.drawable.img1);
-return;
-}
-switch (index)
-{
-case "1":
-imageView.setImageResource(R.drawable.img1);
-break;
-case "2":
-imageView.setImageResource(R.drawable.img2);
-break;
-case "3":
-imageView.setImageResource(R.drawable.img3);
-break;
-case "4":
-imageView.setImageResource(R.drawable.img4);
-break;
-}
-}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            String imgindex= bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+    private void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.ImageViewCountry);
+        switch (index){
+            case "1":
+                imageView.setImageResource(R.drawable.img);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.h2);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.img);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.h2);
+                break;
+        }
+    }
 }
